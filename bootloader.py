f12bd2381185c733c5cf222232d828f16e68771d
NikosJawa <jawa18102001@gmail.com>
2026-03-24 23:44:43 +0200

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -97,25 +97,8 @@ def get_isotp_conn():
 
 
 def sboot_pwm():
-    import time
-    import wavePWM
-
-    GPIO = [12, 13]
-
-    if not pi.connected:
-        exit(0)
-
-    pwm = wavePWM.PWM(pi)  # Use default frequency
-
-    pwm.set_frequency(3210)
-    cl = pwm.get_cycle_length()
-    pwm.set_pulse_start_in_micros(13, cl / 1)
-    pwm.set_pulse_length_in_micros(13, cl / 2)
-
-    pwm.set_pulse_start_in_micros(12, 3 * cl / 4)
-    pwm.set_pulse_length_in_micros(12, cl / 4)
-    pwm.update()
-    return pwm
+    print("PWM handled by ESP32 (external)")
+    return type('', (), {'cancel': lambda self: None})()
 
 
 def reset_ecu():
